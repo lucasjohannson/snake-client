dff43724d87bdb8bcd42e2b08189af16dd3723af
lucas <lucas.johannson1@ucalgary.ca>
2021-06-02 17:26:28 +0000

    moved the connect function to the client

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: "50541"
+
+  });
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Connected !")
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = {
+  connect
+}
