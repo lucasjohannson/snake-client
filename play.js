6722c65cb82ad03a57b6d03bd6b7a015cee2136a
lucas <lucas.johannson1@ucalgary.ca>
2021-06-02 18:35:29 +0000

    ctrl c

--- play.js
+++ play.js
@@ -2,6 +2,23 @@ const {connect} = require("./client");
 
 const net = require("net");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
 // establishes a connection with the game server
 // const connect = function () {
 //   const conn = net.createConnection({
@@ -26,5 +43,6 @@ const net = require("net");
 // };
 
 console.log("Connecting ...");
+setupInput()
 connect();
 
